fb6e373117e4ab32aadc4d0a4a299d7f4d00d431
Jonnthan3visan <joptrabajo@hotmail.com>
2024-11-19 23:39:27 -0300

    cambio de poo a estructurado

--- BaseDeDatos.js
+++ BaseDeDatos.js
@@ -1,67 +1,90 @@
-import { TABLA_REGISTRO_CLIENTE, AGREGAR_CLIENTE_REGISTRO_CLIENTE, ELIMINAR_CLIENTE_TABLA_CLIENTE } from './constante/const.js';
+import { TABLA_REGISTRO_CLIENTE, AGREGAR_CLIENTE, ELIMINAR_CLIENTE_TABLA_CLIENTE } from './constante/const.js';
 import { createConnection } from "mysql2/promise";
-class BaseDeDatos {
-    /**
-     * @returns la conexion con la base de datos
-     */
-    async CrearConexionDB() {
-        try {
-            const conexionDB = await createConnection({
-                host: "localhost",
-                user: "root",
-                database: "gimnasio",
-                password: "kukito"
-            });
-            console.log("Conexión exitosa a la base de datos.");
-            return conexionDB;
-        } catch (error) {
-            console.error("Error al conectar a la base de datos:", error);
-        }
-    }
-    /**
-     * cierra la conexion la base de datos
-     * @returns cierra conexion con la base de datos
-     */
-    async cerraConexionDb(conexionDB) {
-        try {
-            await conexionDB.end();
-            console.log("Conexión cerrada correctamente.");
-        } catch (error) {
-            console.error("Error al cerrar la conexión:", error);
-        }
-    }
-    /**
-     * @returns la tabla completa de Registro Cliente 
-     */
-    async mostrarRegistroClienteDB(conexionDB) {
-        try {
-            const [rows] = await conexionDB.query(TABLA_REGISTRO_CLIENTE);
-            return rows;
-        } catch (error) {
-            console.error("Error al ejecutar la consulta de registroClienteDB:", error);
-        }
-    }
-    /**
-     *  agrega un registro
-     */
-    async agregaRegistroCliente(conexionDB, nombre, apellido, telefono, mail) {
-        try {
-            await conexionDB.query(AGREGAR_CLIENTE_REGISTRO_CLIENTE, [nombre, apellido, telefono, mail]);
-            console.log("Cliente agregado correctamente.");
-        } catch (error) {
-            console.error("Error al agregar cliente:", error);
-        }
-    }
 
-    async elimanarCliente(conexionDB, datoUusario) {
-        try {
-            this.mostrarRegistroClienteDB()
-            await conexionDB.query(ELIMINAR_CLIENTE_TABLA_CLIENTE[datoUusario])
-        } catch (error) {
-            console.error("error al eliminar cliente", error)
-        }
+/**
+ * crea la conexion con la base de datos
+ * @returns la conexion con la base de datos
+ */
+async function crearConexionDB() {
+    try {
+        const conexionDB = await createConnection({
+            host: "localhost",
+            user: "root",
+            database: "gimnasio",
+            password: "kukito"
+        });
+        console.log("Conexión exitosa a la base de datos.");
+        return conexionDB;
+    } catch (error) {
+        console.error("Error al conectar a la base de datos:", error);
+    }
+}
+/**
+ * cierra la conexion con la base de datos
+ * @param {Object} conexionDB del usuario
+ */
+async function cerraConexionDb(conexionDB) {
+    try {
+        await conexionDB.end();
+        console.log("Conexión cerrada correctamente.");
+    } catch (error) {
+        console.error("Error al cerrar la conexión:", error);
+    }
+}
 
+/**
+ * realiza la consulta de mostrar la tabla de cliente registrados
+ * @param {Object} conexionDB del usuario 
+ * @returns el registro de clientes
+ */
+async function mostrarRegistroClienteDB(conexionDB) {
+    try {
+        const [rows] = await conexionDB.query(TABLA_REGISTRO_CLIENTE);
+        return rows;
+    } catch (error) {
+        console.error("Error al ejecutar la consulta de registroClienteDB:", error);
+    }
+}
+/**
+ * agrega un cliente a la tabla registro_cliente
+ * @param {Object} conexionDB del usuario
+ * @param {String} nombre del nuevo cliente
+ * @param {String} apellido del nuevo cliente
+ * @param {Number} dni del nuevo cliente
+ * @param {String} mail del nuevo cliente
+ * @param {Number} telefono del nuevo cliente
+ */
+async function agregaRegistroCliente(conexionDB, nombre, apellido, dni, mail, telefono) {
+    try {
+        await conexionDB.query(AGREGAR_CLIENTE, [nombre, apellido, dni, mail, telefono
+        ]);
+        console.log("Cliente agregado correctamente.");
+    } catch (error) {
+        console.error("Error al agregar cliente:", error);
+    }
+}
+
+/**
+ * elimana un cliente de la base de datos por el dni
+ * @param {Object} conexionDB del usuario 
+ * @param {*} dni del usuario a eliminar
+ */
+async function elimanarCliente(conexionDB, dni) {
+    try {
+        await conexionDB.query(TABLA_REGISTRO_CLIENTE)
+        await conexionDB.query(ELIMINAR_CLIENTE_TABLA_CLIENTE[dni])
+    } catch (error) {
+        console.error("error al eliminar cliente", error)
     }
 
 }
-export default BaseDeDatos;
+
+export default BaseDeDatos = {
+    conectarDB: crearConexionDB(),
+    cerrarConexion: cerraConexionDb(conexionDB),
+    mostrarCliente: mostrarRegistroClienteDB(conexionDB),
+    agregarCliente: agregaRegistroCliente(conexionDB, nombre, apellido, telefono, mail),
+    eliminarCliente: elimanarCliente(conexionDB, dni)
+
+
+};
--- constante/const.js
+++ constante/const.js
@@ -1,10 +1,10 @@
 const TABLA_REGISTRO_CLIENTE = "SELECT * FROM Registro_Cliente";
-const AGREGAR_CLIENTE_REGISTRO_CLIENTE = `INSERT INTO Registro_Cliente(Nombre,Apellido,Telefono,Mail) values(?,?,?,?)`;
-const ELIMINAR_CLIENTE_TABLA_CLIENTE = "DELETE FROM Registro_Cliente where ID= ?"
+const AGREGAR_CLIENTE = `INSERT INTO Registro_Cliente(Nombre,Apellido,DNI, Mail,Telefono) values(?,?,?,?,?)`;
+const ELIMINAR_CLIENTE_TABLA_CLIENTE = "DELETE FROM Registro_Cliente where DNI= ?"
 
 
 export {
     TABLA_REGISTRO_CLIENTE,
-    AGREGAR_CLIENTE_REGISTRO_CLIENTE,
+    AGREGAR_CLIENTE,
     ELIMINAR_CLIENTE_TABLA_CLIENTE
 }
